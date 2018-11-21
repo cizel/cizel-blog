35b1cebd99bf9b47235d85be6042cdcc0b903ba3
cizel <i@cizel.cn>
2018-11-21 13:56:27 +0800

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-rm -rf ./themes/*
-git submodule update --init --recursive
+git clone https://github.com/cizel/hugo-theme-even themes/even
 hugo -t even
 rsync -avz ./public/ /home/work/data/www/www.cizel.cn/
