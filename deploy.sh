8fe646934b727d3ae573e842f422a56fd02ade85
cizel <i@cizel.cn>
2020-10-04 16:43:37 +0800

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 rm -rf themes/*
-git clone https://github.com/cizel/hugo-theme-even themes/even
+git clone https://github.com/cizel/theme-even themes/even
 hugo -t even
 rsync -avz ./public/ /home/work/data/www/www.cizel.cn/
